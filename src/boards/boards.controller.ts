b18306300bd44857ff1696ee0dbc1a72e039ffb9
Song Ahyeon <song@5works.co.kr>
2023-03-20 15:59:48 +0900

    add service
    - getAllBoards

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,9 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService){}
 
-    // this.boardsService.getAllBoards()
+    // request method: GET, POST, PUT, DELETE, ...
+    // Get 요청이 왔을 경우 getAllBoard의 return값(this.boardsService.getAllBoards)을 응답함
+    @Get()
+    getAllBoard() {
+        return this.boardsService.getAllBoards();
+    }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    // 게시판 데이터 담는 변수
+    // 다른 컴포넌트에서 boards라는 배열값을 수정할 수 있기 때문에 접근제한자 private 사용
+    private boards = [];
+
+    /**
+     * 모든 게시물의 데이터를 가져오는 기능 
+     * @returns private boards(boards의 모든 데이터)를 넘겨줌
+     */
+    getAllBoards() {
+        return this.boards;
+    }
+}
