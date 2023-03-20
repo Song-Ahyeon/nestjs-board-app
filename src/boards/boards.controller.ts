62b5790a53c8db468376624575bb4525e05696c4
Song Ahyeon <song@5works.co.kr>
2023-03-20 17:13:42 +0900

    add POST - createBoard

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
@@ -11,5 +11,17 @@ export class BoardsController {
     @Get('/')
     getAllBoard():Board[] {
         return this.boardsService.getAllBoards();
+    }
+
+    // title, description을 인자로 넘겨줘서 service의 createBoard를 실행시키는건데... 어떻게?
+    @Post()
+    createBoard(
+        // 전부 가져오려면 @Body() body
+        @Body('title') title: string,
+        @Body('description') description: string
+    ): Board {  //return type 지정
+        // console.log('title', title);
+        // console.log('description', description);
+        return this.boardsService.createBoard(title, description);
     }
 }
