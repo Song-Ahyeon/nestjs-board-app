acbbc3240c1759625759fbb7a4d07a04105f1ec6
Song Ahyeon <song@5works.co.kr>
2023-03-21 16:55:55 +0900

    fix: entity dependency err
    add get, post

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -8,44 +9,15 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 export class BoardsController {
     constructor(private boardsService: BoardsService){}
 
-    // request method: GET, POST, PUT, DELETE, ...
-    // Get 요청이 왔을 경우 getAllBoard의 return값(this.boardsService.getAllBoards)을 응답함
-    // @Get('/')
-    // getAllBoard():Board[] {
-    //     return this.boardsService.getAllBoards();
-    // }
+    @Get('/:id')
+    getBoardById(@Param('id') id: number): Promise<Board> {
+        return this.boardsService.getBoardById(id);
+    }
 
-    // // title, description을 인자로 넘겨줘서 service의 createBoard를 실행시키는건데... 어떻게?
-    // @Post()
-    // @UsePipes(ValidationPipe)
-    // createBoard(
-    //     @Body() createBoardDto: CreateBoardDto): Board {  //return type 지정
-    //     return this.boardsService.createBoard(createBoardDto);
-    // }
+    @Post()
+    @UsePipes(ValidationPipe)
+    createBoard(@Body() CreateBoardDto: CreateBoardDto): Promise<Board> {
+        return this.boardsService.createBoard(CreateBoardDto);
+    }
 
-    // // query가 parameter보다 아래에 있을 경우 인식하지 못함
-    // // boards/search?title={title}
-    // @Get('/search')
-    // getBoardByTitle(@Query('title') title): Board {
-    //     return this.boardsService.getBoardByTitle(title)
-    // }
-    
-    // // boards/{id}
-    // @Get('/:id')
-    // getBoardById(@Param('id') id: string): Board {
-    //     return this.boardsService.getBoardById(id);
-    // }
-
-    // @Delete('/:id')
-    // deleteBoard(@Param('id') id: string) {
-    //     return this.boardsService.deleteBoard(id)
-    // }
-
-    // @Patch('/:id/status')
-    // updateBoardStatus(
-    //     @Param('id') id: string,
-    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-    // ) {
-    //     return this.boardsService.updateBoardStatus(id, status)
-    // }
 }
