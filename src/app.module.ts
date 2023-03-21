842ef3022a5ae6ee02e320d5462baebb71be05a9
Song Ahyeon <song@5works.co.kr>
2023-03-21 14:52:08 +0900

    set TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { BoardsModule } from './boards/boards.module';
+import { typeORMConfig } from './boards/configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [BoardsModule,
+  TypeOrmModule.forRoot(typeORMConfig),
+  ],
 })
 export class AppModule {}
